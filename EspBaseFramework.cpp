5a18d8d7ba3c3d5b8d6c4d3475c8d50f97fde51e
madHatter1993 <alexw@projectopal.co>
2024-10-23 22:18:04 +0100

    added check for no ssid and change function signiture to return bool

--- EspBaseFramework.cpp
+++ EspBaseFramework.cpp
@@ -160,10 +160,7 @@ void Framework::clearEepromAdminPass(AsyncWebServerRequest *request) {
  * Sets Configuration password in EEPROM
  */
 void Framework::setAdminPass(AsyncWebServerRequest *request) {
- // Check if the admin pass is NOT set
   if(eepromController.isAdminPassSet() != "1"){
-  
-    // Get input credentials
     String pass = request->arg("pass");
     String confirm = request->arg("confirm");
 
@@ -173,7 +170,6 @@ void Framework::setAdminPass(AsyncWebServerRequest *request) {
     }
   }
   
-  // Send back to main root page to login 
   request->redirect("/");
   request->send(302, "text/plain", "");
   
@@ -250,9 +246,8 @@ void Framework::begin() {
 
    String ssid = eepromController.eepromGetWifiSsid();
    String pass = eepromController.eepromGetWifiPass();
-   wifiController.wifiConnTimer(WIFI_CON_WAIT, ssid, pass, DEVICE_NAME);
 
-   if(WiFi.status() != WL_CONNECTED){
+   if(!wifiController.wifiConnTimer(WIFI_CON_WAIT, ssid, pass, DEVICE_NAME) || WiFi.status() != WL_CONNECTED){
       setupCredsRoutine();
       #ifdef SERIAL_DEBUGGING
       Serial.println("please connect to WiFi");
@@ -273,10 +268,9 @@ void Framework::begin() {
  * Main Loop
  */
 void Framework::run() {
-    dnsServer.processNextRequest();
-
+    
     if(WiFi.status() != WL_CONNECTED){
-      // Do Nothing
+      dnsServer.processNextRequest();
     }else{
      otaController.handleOTA();
      //mdnsController.loopHandle();
